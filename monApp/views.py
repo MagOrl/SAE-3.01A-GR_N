c648d672dc3b12d28426d6ba1656974b0a7fcdee
ZDrame28 <zakharia.drame@etu.univ-orleans.fr>
2025-09-11 11:53:58 +0200

    Modification des fichier pour initialisation

--- monApp/views.py
+++ monApp/views.py
@@ -1,9 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
+from .app import app
 
 @app.route('/')
-def index( ) :
+def index():
     return "Hello world !"
 
-if __name__== "__main__" :
-    app.run( )
+if __name__ == "__main__":
+    app.run()
