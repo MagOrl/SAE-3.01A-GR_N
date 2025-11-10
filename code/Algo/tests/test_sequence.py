c4caa6dbb2b5c2e515b163b975206c5b0b7da51b
ZDrame28 <zakharia.drame@etu.univ-orleans.fr>
2025-11-10 16:43:00 +0100

    Question 2 et 3 avec tests

---
+++ code/Algo/tests/test_sequence.py
@@ -0,0 +1,27 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import pytest
+from exploitation_sequence import mutation_par_remplacement,mutation_par_insertion,mutation_par_deletion
+
+sequence_test= "ATCGGCTAATGCCGTAACGTTAGC" #séquence ADN test
+p = 0.4 #taux de mutation test
+
+#Question 2
+
+def test_mutation_par_remplacement():
+    #sequence_test = genere_adn(20)
+    res = mutation_par_remplacement(sequence_test,p)
+    assert len(res)==len(sequence_test)
+
+#Question3
+
+def mutation_par_insertion():
+    #sequence_test = genere_adn(15)
+    res = mutation_par_insertion(sequence_test,p)
+    assert len(res)>=len(sequence_test)
+
+def mutation_par_deletion():
+    #sequence_test = genere_adn(30)
+    res = mutation_par_deletion(sequence_test,p)
+    assert len(res)<=len(sequence_test)
