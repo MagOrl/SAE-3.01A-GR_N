78e392394b3c4dbee23d1ebcf2bc7c9450a83ccf
ZDrame28 <zakharia.drame@etu.univ-orleans.fr>
2025-11-24 18:27:21 +0100

    creation des templates gestion_changement et gestion_maintenance(front), ajout de leur route respective, de leur styles et ajout de la redirection via la page d'accueil du menu technicien

--- monApp/views.py
+++ monApp/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from .app import app
+from .models import *
 
 @app.route('/')
 @app.route('/index/')
@@ -10,9 +11,20 @@ def index():
 def admin():
     return render_template("home_admin.html")
 
+
 @app.route("/technicien/")
 def technicien():
     return render_template("accueil_technicien.html")
+
+@app.route("/gestion_changement_technicien/")
+def gestion_changement_technicien():
+    return render_template("gestion_changement_technicien.html")
+
+@app.route("/gestion_maintenance/")
+def gestion_maintenance():
+    # Récupère toutes les plateformes et prépare les tuples (id_pla, nom_pla, jours_av_mainte)
+    plateformes = [(p.id_pla, p.nom_pla, p.jours_av_mainte) for p in Plateforme.query.all()]
+    return render_template("gestion_maintenance.html", plateformes=plateformes)
 
 if __name__ == "__main__":
     app.run()
