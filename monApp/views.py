d93099c1f4ad13f6c0c1ff9baa7f7ba10ade2b09
tdanzin <titouan.danzin@etu.univ-orleans.fr>
2025-11-24 15:47:09 +0100

    Réalisation de la page d'accueil de Chercheur

--- monApp/views.py
+++ monApp/views.py
@@ -1,10 +1,26 @@
-from flask import render_template
+from flask import Flask,render_template
 from .app import app
 
 @app.route('/')
 @app.route('/index/')
 def index():
     return render_template("nav_bar.html")
+
+@app.route('/chercheur/')
+def chercheur_accueil():
+    return render_template("Chercheur_Accueil.html")
+
+@app.route('/chercheur/campagne/')
+def chercheur_campagne():
+    return render_template("Chercheur_Campagne.html")
+
+@app.route('/chercheur/echantillon/')
+def chercheur_echantillon():
+    return render_template("Chercheur_Echantillon.html")
+
+@app.route('/chercheur/sequence/')
+def chercheur_sequence():
+    return render_template("Chercheur_Sequence.html")
 
 if __name__ == "__main__":
     app.run()
