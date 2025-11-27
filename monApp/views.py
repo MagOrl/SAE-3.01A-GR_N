e8d4b508d13287c8639bc87a1320c110155f5762
MagOrl <m4g0o0@proton.me>
2025-11-27 16:25:52 +0100

    Arrangement bugs (un chercheur pouvais acceder au pages admin)

--- monApp/views.py
+++ monApp/views.py
@@ -1,13 +1,13 @@
-from flask import render_template, url_for, request, redirect
+from flask import render_template, url_for, request, redirect, Response, session
 from .app import app
 from monApp.forms import LoginForm
-from flask_login import login_user
-
-
+from flask_login import login_user,login_required,logout_user
+from monApp.models import User
 @app.route('/')
+@app.route('/index')
 @app.route("/login/", methods=("GET", "POST"))
 def login():
-    if len(request.args) == 0:
+    if session["user"] is None:
         unForm = LoginForm()
         unUser = None
         if not unForm.is_submitted():
@@ -16,33 +16,55 @@ def login():
             unUser = unForm.get_authenticated_user()
             if unUser:
                 login_user(unUser)
+                session["user"] = unUser
                 next = unForm.next.data or url_for(f"{unUser.Role}_accueil")
                 return redirect(next)
-    return render_template("login.html", form=unForm)
-
-
+        return render_template("login.html", form=unForm)
+    else:
+        return redirect(url_for(f"{session["user"].Role}_accueil"))
+@app.route ("/logout/")
+def logout():
+    logout_user()
+    return redirect ( url_for ('login'))
 @app.route('/chercheur/')
+@app.errorhandler(401)
+@login_required
 def chercheur_accueil():
+    if session["user"].Role != 'chercheur':
+        return render_template("access_denied.html",error ='401', reason="Vous n'avez pas les droits d'accès à cette page.")
+
     return render_template("Chercheur_Accueil.html")
 
-
 @app.route('/chercheur/campagne/')
+@app.errorhandler(401)
+@login_required
 def chercheur_campagne():
+    if session["user"].Role != 'chercheur':
+        return render_template("access_denied.html",error ='401', reason="Vous n'avez pas les droits d'accès à cette page.")
     return render_template("Chercheur_Campagne.html")
 
-
 @app.route('/chercheur/echantillon/')
+@app.errorhandler(401)
+@login_required
 def chercheur_echantillon():
+    if session["user"].Role != 'chercheur':
+        return render_template("access_denied.html",error ='401', reason="Vous n'avez pas les droits d'accès à cette page.")
     return render_template("Chercheur_Echantillon.html")
 
-
 @app.route('/chercheur/sequence/')
+@login_required
+@app.errorhandler(401)
 def chercheur_sequence():
+    if session["user"].Role != 'chercheur':
+        return render_template("access_denied.html",error ='401', reason="Vous n'avez pas les droits d'accès à cette page.")
     return render_template("Chercheur_Sequence.html")
 
-
 @app.route("/admin/")
-def admin():
+@app.errorhandler(401)
+@login_required
+def admin_accueil():
+    if session["user"].Role != 'admin':
+            return render_template("access_denied.html",error ='401', reason="Vous n'avez pas les droits d'accès à cette page.")
     return render_template("home_admin.html")
 
 
