938dc2df9beb06224f7713358e24a348f2d97527
ZDrame28 <zakharia.drame@etu.univ-orleans.fr>
2025-11-27 17:37:06 +0100

    Ajout de la methode loaddb

--- monApp/commands.py
+++ monApp/commands.py
@@ -4,26 +4,39 @@ from hashlib import sha256
 from .app import app, db
 import sqlalchemy
 from sqlalchemy import text
+from .models import *
 
-lg = logging.getLogger(__name__)
-CONNEXION = None
-try:
-    CONNEXION = sqlalchemy.create_engine('mysql://'+"arsamerzoev"+':'+"arsamerzoev"+'@'+"servinfo-maria"+'/'+"DBarsamerzoev")
-except Exception as err:
-    print(err)
-    
-    
+
+lg = logging.getLogger(__name__)    
 
 @app.cli.command()
 def syncdb() -> None:
     """
     Crée les tables de la base de données.
     """
-    files = ["CREATION.sql","TRIGGER.sql"]
-    for file in files:    
-        path = os.path.join("code/BD/", file)
-        with CONNEXION.connect() as con:
-            with open(path,'r') as fic:
-                query = text(fic.read())
-                con.execute(query)
+    db.create_all()
     lg.warning('Base de donnée synchronisée!')
+
+
+@app.cli.command()
+def loaddb() -> None:
+    """
+    Charge les données de peuplement.
+    """
+    import os
+    
+    # Chemin vers le fichier PEUPLEMENT.sql
+    # On suppose que le fichier est dans code/BD/PEUPLEMENT.sql par rapport à la racine du projet
+    file_path = os.path.join(os.getcwd(), 'code', 'BD', 'PEUPLEMENT.sql')
+    
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as f:
+            sql_commands = f.read().split(';')
+        
+        for command in sql_commands:
+            if command.strip():
+                db.session.execute(text(command))
+        db.session.commit()
+        lg.warning('Données chargées depuis PEUPLEMENT.sql !')
+    else:
+        lg.error(f'Fichier non trouvé : {file_path}')
